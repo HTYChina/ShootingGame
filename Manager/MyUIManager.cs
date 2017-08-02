437fcef54f9e684e7689af629fbeb606cad70950
HTY <2763085675@qq.com>
2017-08-02 12:36:07 +0800

    Base Manager
    泛型。。。
    UIManager

---
+++ Manager/MyUIManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MyUIManager : ObjectInstance<MyUIManager>
+{
+	public static MyUIManager myinstance;
+
+	void Awake(){
+        myinstance = this;
+		SetInstance (this);
+	}
+
+	public override void SetInstance(MyUIManager t){
+		base.SetInstance (t);
+	}
+}
---
+++ Manager/ObjectInstance.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInstance<T> : MonoBehaviour {
+	private static T instance;
+
+	public static T Instance{
+		get{
+			return instance;
+		}
+	}
+
+	public virtual void SetInstance(T t){
+		instance = t;
+	}
+}
